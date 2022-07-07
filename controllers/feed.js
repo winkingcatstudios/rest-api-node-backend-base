22c7c34d843f70ef25535c2a969f497ca9522024
winkingcat <dankercher@gmail.com>
2022-07-07 10:35:30 -0400

    added delete post functionality

--- controllers/feed.js
+++ controllers/feed.js
@@ -32,7 +32,7 @@ exports.createPost = (req, res, next) => {
     error.statusCodfe = 422;
     throw error;
   }
-  const imageUrl = req.file.path.replace("\\","/");;
+  const imageUrl = req.file.path.replace("\\", "/");
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
@@ -90,7 +90,7 @@ exports.updatePost = (req, res, next) => {
   const content = req.body.content;
   let imageUrl = req.body.image;
   if (req.file) {
-    imageUrl = req.file.path.replace("\\","/");;
+    imageUrl = req.file.path.replace("\\", "/");
   }
   if (!imageUrl) {
     const error = new Error("No file picked");
@@ -114,6 +114,31 @@ exports.updatePost = (req, res, next) => {
     })
     .then((result) => {
       res.status(200).json({ message: "Post updated", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404;
+        throw error;
+      }
+      // check logged in user
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Post deleted" });
     })
     .catch((err) => {
       if (!err.statusCode) {
